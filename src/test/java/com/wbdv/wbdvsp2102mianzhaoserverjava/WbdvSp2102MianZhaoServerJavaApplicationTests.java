78777053aed9bad8f0f402fcf56a380c6318eaf8
miaaaz <zhao.mia@northeastern.edu>
2021-01-27 15:48:00 +0800

    initialize project

---
+++ src/test/java/com/wbdv/wbdvsp2102mianzhaoserverjava/WbdvSp2102MianZhaoServerJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wbdv.wbdvsp2102mianzhaoserverjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WbdvSp2102MianZhaoServerJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
