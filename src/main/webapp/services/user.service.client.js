8c536fbb511d07f8942f040f4587c7ee2658fc99
miaaaz <zhao.mia@northeastern.edu>
2021-02-07 22:07:51 +0800

    Create update user function

--- src/main/webapp/services/user.service.client.js
+++ src/main/webapp/services/user.service.client.js
@@ -1,46 +1,42 @@
 function AdminUserServiceClient() {
-    this.createUser = createUser;
-    this.findAllUsers = findAllUsers;
-    this.findUserById = findUserById;
-    this.deleteUser = deleteUser;
-    this.updateUser = updateUser;
-    this.url = 'https://wbdv-generic-server.herokuapp.com/api/001029592/users';
-    var self = this;
+  this.createUser = createUser;
+  this.findAllUsers = findAllUsers;
+  this.findUserById = findUserById;
+  this.deleteUser = deleteUser;
+  this.updateUser = updateUser;
+  this.url = 'https://wbdv-generic-server.herokuapp.com/api/001029592/users';
+  var self = this;
 
-    function createUser(user) {
-        return fetch(self.url, {
-            method: 'POST',
-            headers: {
-                'content-type': 'application/json'
-            },
-            body: JSON.stringify(user)
-        }).then(function(response) {
-            return response.json()
-        })
-    }
+  function createUser(user) {
+    return fetch(self.url, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    }).then(response => response.json())
+  }
 
-    function findAllUsers() {
-        return fetch(self.url).then(function (response) {
-            return response.json()
-        })
-    }
-    function findUserById(userId) {
+  function findAllUsers() {
+    return fetch(self.url).then(response => response.json())
+  }
 
-    }
-    function updateUser(userId, user) {
+  function findUserById(userId) {
+  }
 
+  function updateUser(userId, user) {
+    return fetch(`${self.url}/${userId}`, {
+      method: 'PUT',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    }).then(response => response.json())
+  }
 
-    }
-    // function deleteUser(userId) {
-    //     return fetch(`${self.url}/${userId}`,
-    //         {method: 'DELETE'}).then(function(response) {
-    //             return response.json()
-    //     })
-    // }
-
-    function deleteUser(userId) {
-        return fetch(`${self.url}/${userId}`,
-            {method: 'DELETE'})
-    }
+  function deleteUser(userId) {
+    return fetch(`${self.url}/${userId}`,
+        {method: 'DELETE'})
+  }
 
 }
--- src/main/webapp/user-admin/user-admin.controller.client.js
+++ src/main/webapp/user-admin/user-admin.controller.client.js
@@ -3,16 +3,14 @@
   var $usernameFld, $passwordFld;
   var $firstNameFld, $lastNameFld, $roleFld;
   var $removeBtn, $editBtn, $createBtn;
-  var $userRowTemplate, $tbody;
+  var $userRowTemplate, $updateBtn, $tbody;
+  var selectedUser;
   var userService =  new AdminUserServiceClient()
 
 
   $(main)
 
-  var users = [
-    // {username: "ada", password: "", firstName: "Ada", lastName: "Lovelace", role: "Faculty"},
-    // {username: "sfa", password: "", firstName: "Bo", lastName: "Chen", role: "Faculty"},
-  ]
+  var users = []
 
   function main() {
     $userRowTemplate = $("#wbdv-tbody")
@@ -25,15 +23,86 @@
     $editBtn = $(".wbdv-edit")
     $createBtn = $(".wbdv-create")
     $removeBtn = $(".wbdv-remove")
+    $updateBtn = $(".wbdv-update")
 
     findAllUsers()
 
-    // $createBtn.click(createUser)
-    // userService.findAllUsers().then(function (allUsers) {
-    //   users = allUsers
-    //   renderUsers(users)
-    // })
+    // Listen to create user button
+    $createBtn.click(createUser)
+    $updateBtn.click(updateUser)
 
+  }
+
+  function createUser() {
+    var newUser = {
+      username: $usernameFld.val(),
+      password: $passwordFld.val(),
+      firstName: $firstNameFld.val(),
+      lastName: $lastNameFld.val(),
+      role: $roleFld.val()
+    }
+
+    // Clear the input field
+    $usernameFld.val('')
+    $passwordFld.val('')
+    $firstNameFld.val('')
+    $lastNameFld.val('')
+
+    userService.createUser(newUser).then(function (actualUser) {
+      users.push(actualUser)
+      renderUsers(users)
+    })
+  }
+
+
+  function deleteUser(event) {
+    var button = $(event.target)
+    var index = button.attr("id")
+    var theUserId = users[index]._id
+
+    userService.deleteUser(theUserId).then(function (status){
+      users.splice(index, 1)
+      renderUsers(users)
+    })
+  }
+
+
+  function selectUser(event) {
+    var index = $(event.target).attr("id")
+    selectedUser = users[index]
+    $usernameFld.val(selectedUser.username)
+    $passwordFld.val(selectedUser.password)
+    $firstNameFld.val(selectedUser.firstName)
+    $lastNameFld.val(selectedUser.lastName)
+    $roleFld.val(selectedUser.role)
+  }
+
+  function updateUser() {
+    selectedUser.username =  $usernameFld.val()
+    selectedUser.password =  $passwordFld.val()
+    selectedUser.firstName = $firstNameFld.val()
+    selectedUser.lastName = $lastNameFld.val()
+    selectedUser.role =  $roleFld.val()
+
+    // Clear the input field
+    $usernameFld.val('')
+    $passwordFld.val('')
+    $firstNameFld.val('')
+    $lastNameFld.val('')
+
+    userService.updateUser(selectedUser._id, selectedUser)
+      .then(function (status) {
+        var index = users.findIndex(user => user._id === selectedUser._id)
+        users[index] = selectedUser
+        renderUsers(users)
+      })
+  }
+
+  function findAllUsers() {
+    userService.findAllUsers().then(function (allUsers) {
+      users = allUsers
+      renderUsers(users)
+    })
   }
 
   function renderUsers(users) {
@@ -51,68 +120,15 @@
             <button class="float-right">
               <i id="${i}" class="wbdv-remove fa fa fa-times wbdv-remove wbdv-icon"></i>
             </button>
-          </td>
-          <td class="wbdv-actions" style="white-space: nowrap">
             <button class="float-right">
               <i id="${i}" class="wbdv-edit fa fa fa-pencil wbdv-edit wbdv-icon"></i>
             </button>
-          </td>
         </tr>
       `
       )
     }
     $(".wbdv-remove").click(deleteUser)
     $(".wbdv-edit").click(selectUser)
-
-
-  }
-
-  function createUser() {
-    var newUser = {
-      username: $usernameFld.val(),
-      password: $passwordFld.val(),
-      firstName: $firstNameFld.val(),
-      lastName: $lastNameFld.val(),
-      role: $roleFld.val()
-    }
-
-    userService.createUser(newUser).then(function (actualUser) {
-      users.push(actualUser)
-      renderUsers(users)
-    })
-
-
-  }
-
-
-  function deleteUser(event) {
-    var button = $(event.target)
-    var index = button.attr("id")
-    var theUserId = users[index]._id
-    console.log(theUserId)
-
-    userService.deleteUser(theUserId).then(function (status){
-      users.splice(index, 1)
-      renderUsers(users)
-    })
-  }
-
-  function selectUser(event) {
-    var id = $(event.target).attr("id")
-    var selectedUser = users[id]
-    $usernameFld.val(selectedUser.username)
-    $passwordFld.val(selectedUser.password)
-    $firstNameFld.val(selectedUser.firstName)
-    $lastNameFld.val(selectedUser.lastName)
-    $roleFld.val(selectedUser.role)
-
-  }
-
-  function findAllUsers() {
-    userService.findAllUsers().then(function (allUsers) {
-      users = allUsers
-      renderUsers(users)
-    })
   }
 
 
